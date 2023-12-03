0cb3461997fc9f3f77229167cbf8881c1beb62c3
cpascasio <christian_james_pascasio@dlsu.edu.ph>
2023-12-03 23:59:55 +0800

    moved usercontext

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
-import { useUser } from "../../../public/usercontext.jsx";
+import { useUser } from "../../../src/context/UserContext.jsx";
 import "./login.css";
 import { toast } from "react-toastify";
 import useLogin from "../../../hooks/useLogin.js";
